edb0905421787630d708cb3e2942ed4cb65eac44
cimoxx <lukas.slovakia@gmail.com>
2026-08-03 13:01:58 +0200

    Update utils.js

--- js/core/utils.js
+++ js/core/utils.js
@@ -1,1 +1,9 @@
-
+const Utils = {
+    generateId: () => '_' + Math.random().toString(36).substr(2, 9),
+    formatMoney: (amount) => new Intl.NumberFormat('sk-SK', { style: 'currency', currency: 'EUR' }).format(amount),
+    formatDate: (dateString) => {
+        const d = new Date(dateString);
+        return d.toLocaleDateString('sk-SK', { day: '2-digit', month: '2-digit', year: 'numeric' });
+    },
+    vibrate: (ms = 50) => { if (navigator.vibrate) navigator.vibrate(ms); }
+};
