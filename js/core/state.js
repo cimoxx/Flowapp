1e410b56a40ef8c9b10c9b0a0d7eb86f80cdbd60
cimoxx <lukas.slovakia@gmail.com>
2026-08-03 13:02:13 +0200

    Update state.js

--- js/core/state.js
+++ js/core/state.js
@@ -1,13 +1,20 @@
 const State = {
     db: JSON.parse(localStorage.getItem('f_db_v20')) || [],
-    syncQueue: JSON.parse(localStorage.getItem('f_sync_q_v20')) || [],
     categories: JSON.parse(localStorage.getItem('f_cats_v20')) || [],
-    chartPresets: JSON.parse(localStorage.getItem('f_chart_presets_v20')) || [],
-    curUser: localStorage.getItem('f_last_user') || 'Lukáš',
+    syncQueue: JSON.parse(localStorage.getItem('f_sync_q_v20')) || [],
+    currentUser: localStorage.getItem('f_last_user') || 'Lukáš',
     
     save() {
         localStorage.setItem('f_db_v20', JSON.stringify(this.db));
-        localStorage.setItem('f_sync_q_v20', JSON.stringify(this.syncQueue));
         localStorage.setItem('f_cats_v20', JSON.stringify(this.categories));
+        localStorage.setItem('f_sync_q_v20', JSON.stringify(this.syncQueue));
+    },
+    addTransaction(tx) {
+        this.db.push(tx);
+        this.save();
+    },
+    deleteTransaction(id) {
+        this.db = this.db.filter(t => t.id !== id);
+        this.save();
     }
 };
