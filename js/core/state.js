fa17dde0e73ecf3ec2b925fb251a7f78bc73a089
cimoxx <lukas.slovakia@gmail.com>
2026-08-03 17:04:14 +0200

    Update database retrieval and transaction deletion logic

--- js/core/state.js
+++ js/core/state.js
@@ -1,23 +1,29 @@
 const State = {
-    // Ak stará appka používala iný kľúč, zmeň 'f_db_v20' na ten pôvodný
-    db: JSON.parse(localStorage.getItem('f_db_v20')) || [],
+    db: JSON.parse(localStorage.getItem('f_db_v20')) || JSON.parse(localStorage.getItem('flow_db')) || [],
     categories: JSON.parse(localStorage.getItem('f_cats_v20')) || [],
     syncQueue: JSON.parse(localStorage.getItem('f_sync_q_v20')) || [],
     currentUser: localStorage.getItem('f_last_user') || 'Lukáš',
     currentMonth: new Date().getMonth(),
     currentYear: new Date().getFullYear(),
+    filterMode: 'all',
+
+    getNormalizedDb() {
+        return this.db.map(tx => Utils.normalizeTx(tx)).filter(Boolean);
+    },
     
     save() {
         localStorage.setItem('f_db_v20', JSON.stringify(this.db));
         localStorage.setItem('f_cats_v20', JSON.stringify(this.categories));
         localStorage.setItem('f_sync_q_v20', JSON.stringify(this.syncQueue));
     },
+
     addTransaction(tx) {
         this.db.push(tx);
         this.save();
     },
+
     deleteTransaction(id) {
-        this.db = this.db.filter(t => t.id !== id);
+        this.db = this.db.filter(t => (t.id || t._id) !== id);
         this.save();
     }
 };
