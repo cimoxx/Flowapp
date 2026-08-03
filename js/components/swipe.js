5f2a21d769851e7963792c49357d23c28b3a3f61
cimoxx <lukas.slovakia@gmail.com>
2026-08-03 13:03:29 +0200

    Update swipe.js

--- js/components/swipe.js
+++ js/components/swipe.js
@@ -1,7 +1,23 @@
-let touchStartX = 0;
-let touchStartY = 0;
-let isSwiping = false;
+const Swipe = {
+    init(element, onSwipeLeft, onSwipeRight) {
+        let startX = 0, currentX = 0;
+        
+        element.addEventListener('touchstart', e => {
+            startX = e.touches[0].clientX;
+        }, {passive: true});
 
-function initSwipeHandlers(id) {
-    // Obsluha touchstart, touchmove, touchend pre konkrétnu transakciu
-}
+        element.addEventListener('touchmove', e => {
+            currentX = e.touches[0].clientX - startX;
+            if(Math.abs(currentX) > 20) {
+                element.style.transform = `translateX(${currentX}px)`;
+            }
+        }, {passive: true});
+
+        element.addEventListener('touchend', e => {
+            element.style.transform = `translateX(0px)`;
+            if (currentX < -80 && onSwipeLeft) { Utils.vibrate(); onSwipeLeft(); }
+            if (currentX > 80 && onSwipeRight) { Utils.vibrate(); onSwipeRight(); }
+            currentX = 0;
+        });
+    }
+};
