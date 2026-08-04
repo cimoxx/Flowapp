e7bd157fee80f8256ac144a748e9b37dfe79b9ca
cimoxx <lukas.slovakia@gmail.com>
2026-08-04 09:46:07 +0200

    Create app.js

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+(function initApp() {
+    const yearSelect = document.getElementById('filter-year');
+    const currentYear = new Date().getFullYear();
+
+    for (let y = currentYear - 2; y <= currentYear + 2; y++) {
+        const opt = document.createElement('option');
+        opt.value = y;
+        opt.innerText = y;
+        if (y === currentYear) opt.selected = true;
+        yearSelect.appendChild(opt);
+    }
+
+    renderMonthChips();
+    renderCatGrid();
+    processRecurringPayments();
+    renderList();
+
+    if (pendingCatSync) {
+        syncCategories('push');
+    } else {
+        syncCategories('pull');
+    }
+
+    syncTransactions('pull');
+    lucide.createIcons();
+})();
