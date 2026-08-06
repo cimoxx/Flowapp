11417b91a4fca85fd9b43d45089e9063426de186
cimoxx <lukas.slovakia@gmail.com>
2026-08-06 11:08:53 +0200

    Refactor year select population logic
    Clear existing options in year select before populating.

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,12 +2,15 @@
     const yearSelect = document.getElementById('filter-year');
     const currentYear = new Date().getFullYear();
 
-    for (let y = currentYear - 2; y <= currentYear + 2; y++) {
-        const opt = document.createElement('option');
-        opt.value = y;
-        opt.innerText = y;
-        if (y === currentYear) opt.selected = true;
-        yearSelect.appendChild(opt);
+    if (yearSelect) {
+        yearSelect.innerHTML = '';
+        for (let y = currentYear - 2; y <= currentYear + 2; y++) {
+            const opt = document.createElement('option');
+            opt.value = y;
+            opt.innerText = y;
+            if (y === currentYear) opt.selected = true;
+            yearSelect.appendChild(opt);
+        }
     }
 
     renderMonthChips();
